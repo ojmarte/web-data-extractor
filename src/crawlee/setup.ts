cf8c31221de5e12a2e97a4a3289350b592b0a0d7
Osvaldo Marte <o.j.marte@gmail.com>
2023-11-04 20:39:57 -0400

    add new helper method to hoverOnSide

--- src/crawlee/setup.ts
+++ src/crawlee/setup.ts
@@ -3,6 +3,6 @@ import { Configuration } from 'crawlee';
 export const crawleeConfig = new Configuration({
     // Set the 'persistStateIntervalMillis' option to 10 seconds
     // persistStateIntervalMillis: 10_000,
-    defaultBrowserPath: '/mnt/c/Program Files/BraveSoftware/Brave-Browser/Application/brave.exe',
+    // defaultBrowserPath: '/mnt/c/Program Files/BraveSoftware/Brave-Browser/Application/brave.exe',
     headless: false
 });
